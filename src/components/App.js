e7534d82457b6cb7e4fdd943e1a8b3014552cbfc
Joel Coddington <coddingtonjoel@gmail.com>
2021-01-03 18:35:58 -0800

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello from Electron!</h1>
+      <p>React seems to be working just fine!</p>
+    </div>
+  );
+};
+
+export default App;
