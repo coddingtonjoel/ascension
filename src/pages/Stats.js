8141c43be8553dd3a57c41513494e1eb84251e97
Joel Coddington <coddingtonjoel@gmail.com>
2021-01-15 18:42:21 -0800

    Encryption Update

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -7,11 +7,17 @@ import axios from "axios";
 import Profile from "../components/Profile";
 import Loading from "../components/Loading";
 import NotFound from "../components/NotFound";
+import CryptoAES from "crypto-js/aes";
+import CryptoENC from "crypto-js/enc-utf8";
+import envVar from "../context/env-vars.json";
 
 const Stats = () => {
   const [name, setName] = useContext(NameContext);
   const [region, setRegion] = useContext(RegionContext);
   const [content, setContent] = useState(<Loading />);
+
+  let temp2 = CryptoAES.decrypt(envVar.temp, "deleteYasuoFromLoL");
+  temp2 = temp2.toString(CryptoENC);
 
   let id;
   let profileIconId;
@@ -25,104 +31,125 @@ const Stats = () => {
 
   useEffect(() => {
     setContent(<Loading />);
-    // res => basic account information including summoner id
     axios
-      .get(
-        //`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/YepThatsMahogany?api_key=${process.env.KEY}`
-        `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${process.env.KEY}`
-      )
-      .then((res) => {
-        console.log(res);
-        id = res.data.id;
-        profileIconId = res.data.profileIconId;
-        summonerLevel = res.data.summonerLevel;
-
-        // res => summoner's ranked information
+      .get("http://localhost:3001/riot-key", {
+        headers: {
+          authorization: temp2,
+        },
+      })
+      .then((responseData) => {
+        // res => basic account information including summoner id
         axios
           .get(
-            //`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/YepThatsMahogany?api_key=${process.env.KEY}`
-            `https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${process.env.KEY}`
+            `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/YepThatsMahogany?api_key=${responseData.data}`
+            //`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${responseData.data}`
           )
           .then((res) => {
             console.log(res);
-            if (res.data.length > 0) {
-              const soloDuoRank = res.data.filter(
-                (queue) => queue.queueType === "RANKED_SOLO_5x5"
-              );
-              let tier = soloDuoRank[0].tier;
-              tier = tier.toLowerCase();
-              tier = tier.charAt(0).toUpperCase() + tier.slice(1);
-              rank = tier + " " + soloDuoRank[0].rank;
-            } else {
-              rank = "Unranked";
-            }
+            id = res.data.id;
+            profileIconId = res.data.profileIconId;
+            summonerLevel = res.data.summonerLevel;
 
-            // res => summoner's mastery
+            // res => summoner's ranked information
             axios
               .get(
-                //`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/YepThatsMahogany?api_key=${process.env.KEY}`
-                `https://${region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${id}?api_key=${process.env.KEY}`
+                //`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/YepThatsMahogany?api_key=${responseData.data}`
+                `https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${responseData.data}`
               )
               .then((res) => {
-                // list of all raw mastery with ids and unix time rather than consumable data for the user
-                let masteryData = res.data;
+                console.log(res);
+                if (res.data.length > 0) {
+                  const soloDuoRank = res.data.filter(
+                    (queue) => queue.queueType === "RANKED_SOLO_5x5"
+                  );
+                  let tier = soloDuoRank[0].tier;
+                  tier = tier.toLowerCase();
+                  tier = tier.charAt(0).toUpperCase() + tier.slice(1);
+                  rank = tier + " " + soloDuoRank[0].rank;
+                } else {
+                  rank = "Unranked";
+                }
 
-                // get updated patch version so that all Data Dragon data is accurate
+                // res => summoner's mastery
                 axios
-                  .get("https://ddragon.leagueoflegends.com/api/versions.json")
+                  .get(
+                    //`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/YepThatsMahogany?api_key=${responseData.data}`
+                    `https://${region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${id}?api_key=${responseData.data}`
+                  )
                   .then((res) => {
-                    patch = res.data[0];
+                    // list of all raw mastery with ids and unix time rather than consumable data for the user
+                    let masteryData = res.data;
 
+                    // get updated patch version so that all Data Dragon data is accurate
                     axios
                       .get(
-                        `http://ddragon.leagueoflegends.com/cdn/${patch}/data/en_US/champion.json`
+                        "https://ddragon.leagueoflegends.com/api/versions.json"
                       )
                       .then((res) => {
-                        championJSON = res.data.data;
+                        patch = res.data[0];
 
-                        // add object attribute for each champion in mastery linking their champion id to their champion name and image
-                        // effectively links champions.json (from Riot's CDN) to this app
-                        masteryData.map((item) => {
-                          let name;
-                          let iconName;
+                        axios
+                          .get(
+                            `http://ddragon.leagueoflegends.com/cdn/${patch}/data/en_US/champion.json`
+                          )
+                          .then((res) => {
+                            championJSON = res.data.data;
 
-                          for (const [key, value] of Object.entries(
-                            championJSON
-                          )) {
-                            if (value.key == item.championId) {
-                              name = value.name;
-                              iconName = value.image.full;
+                            // add object attribute for each champion in mastery linking their champion id to their champion name and image
+                            // effectively links champions.json (from Riot's CDN) to this app
+                            masteryData.map((item) => {
+                              let name;
+                              let iconName;
+
+                              for (const [key, value] of Object.entries(
+                                championJSON
+                              )) {
+                                if (value.key == item.championId) {
+                                  name = value.name;
+                                  iconName = value.image.full;
+                                }
+                              }
+
+                              // add champion name attribute to masteryData items
+                              // add champion image attribute to masteryData items
+                              item.name = name;
+                              item.img = `http://ddragon.leagueoflegends.com/cdn/${patch}/img/champion/${iconName}`;
+                              return item;
+                            });
+
+                            console.log(masteryData);
+
+                            // gather top 5 champions to display images of
+                            let topFive = [];
+                            for (let i = 0; i < 5; i++) {
+                              topFive.push(masteryData[i]);
                             }
-                          }
 
-                          // add champion name attribute to masteryData items
-                          // add champion image attribute to masteryData items
-                          item.name = name;
-                          item.img = `http://ddragon.leagueoflegends.com/cdn/${patch}/img/champion/${iconName}`;
-                          return item;
-                        });
-
-                        console.log(masteryData);
-
-                        // gather top 5 champions to display images of
-                        let topFive = [];
-                        for (let i = 0; i < 5; i++) {
-                          topFive.push(masteryData[i]);
-                        }
-
-                        setContent(
-                          <Profile
-                            masteryData={masteryData}
-                            id={id}
-                            name={name}
-                            summonerLevel={summonerLevel}
-                            profileIconId={profileIconId}
-                            rank={rank}
-                            topFive={topFive}
-                          />
-                        );
+                            setContent(
+                              <Profile
+                                masteryData={masteryData}
+                                id={id}
+                                name={name}
+                                summonerLevel={summonerLevel}
+                                profileIconId={profileIconId}
+                                rank={rank}
+                                topFive={topFive}
+                              />
+                            );
+                          })
+                          .catch((err) => console.log(err));
                       })
                       .catch((err) => console.log(err));
+                  })
+                  .catch((err) => {
+                    let status = err.response.status;
+                    if (status === 404) {
+                      setContent(<NotFound />);
+                    } else {
+                      setContent(
+                        "An error has occurred. Please restart Ascension."
+                      );
+                    }
                   })
                   .catch((err) => console.log(err));
               })
@@ -135,8 +162,7 @@ const Stats = () => {
                     "An error has occurred. Please restart Ascension."
                   );
                 }
-              })
-              .catch((err) => console.log(err));
+              });
           })
           .catch((err) => {
             let status = err.response.status;
@@ -147,14 +173,7 @@ const Stats = () => {
             }
           });
       })
-      .catch((err) => {
-        let status = err.response.status;
-        if (status === 404) {
-          setContent(<NotFound />);
-        } else {
-          setContent("An error has occurred. Please restart Ascension.");
-        }
-      });
+      .catch((err) => console.log(err));
   }, [name, region]);
 
   return (
